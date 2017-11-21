c13b4fba47a9b55f2fd71b2f890aff0362266dd9
Jason Walsh <rightlag@users.noreply.github.com>
2017-11-21 09:47:13 -0500

    Add enumerated type `State` (#7)
    * Add enumerated type `State`
    
    Closes #6
    
    * Rename 'main' package to 'health'
    
    Without a `main` function, Travis CI fails to build.
    
    See #6

---
+++ health.go
@@ -0,0 +1,46 @@
+package gentry
+
+import (
+	"encoding/json"
+)
+
+type State int
+
+func (s *State) UnmarshalJSON(data []byte) error {
+	var state string
+	if err := json.Unmarshal(data, &state); err != nil {
+		return err
+	}
+	switch state {
+	case "critical":
+		*s = Critical
+	case "passing":
+		*s = Passing
+	case "warning":
+		*s = Warning
+	default:
+		*s = Any
+	}
+	return nil
+}
+
+const (
+	Any State = iota
+	Critical
+	Passing
+	Warning
+)
+
+type Check struct {
+	ID          string `json:"CheckID"`
+	Name        string `json:"Name"`
+	Node        string `json:"Node"`
+	Notes       string `json:"Notes"`
+	Output      string `json:"Output"`
+	ServiceID   string `json:"ServiceID"`
+	ServiceName string `json:"ServiceName"`
+	ServiceTags string `json:"ServiceTags"`
+	Status      State  `json:"Status"`
+}
+
+type Checks []*Check
---
+++ health_test.go
@@ -0,0 +1,38 @@
+package gentry
+
+import (
+	"encoding/json"
+	"testing"
+)
+
+func TestTypeStateDecode(t *testing.T) {
+	// For the sake of brevity, the full response returned from the
+	// 'List Checks in State' endpoint is omitted.
+	// https://www.consul.io/api/health.html#sample-response-3
+	data := []byte(`
+		[
+			{
+				"Status": "any"
+			},
+			{
+				"Status": "critical"
+			},
+			{
+				"Status": "passing"
+			},
+			{
+				"Status": "warning"
+			}
+		]
+	`)
+	var checks Checks
+	if err := json.Unmarshal(data, &checks); err != nil {
+		t.Error(err)
+	}
+	for i, expected := range []State{Any, Critical, Passing, Warning} {
+		actual := checks[i].Status
+		if expected != actual {
+			t.Errorf("main: expected type %d, got %d instead", expected, actual)
+		}
+	}
+}
