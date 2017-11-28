0dd78921ce30b4af55094a735be29b0b5bfc2cde
rightlag <rightlag@gmail.com>
2017-11-28 10:08:50 -0500

    Update health_test.go
    Minify JSON data and declare zero value variable `actual` to
    prevent allocating new memory per iteration.

--- health_test.go
+++ health_test.go
@@ -10,27 +10,15 @@ func TestTypeStateDecode(t *testing.T) {
 	// 'List Checks in State' endpoint is omitted.
 	// https://www.consul.io/api/health.html#sample-response-3
 	data := []byte(`
-		[
-			{
-				"Status": "any"
-			},
-			{
-				"Status": "critical"
-			},
-			{
-				"Status": "passing"
-			},
-			{
-				"Status": "warning"
-			}
-		]
+		[{"Status": "any"}, {"Status": "critical"}, {"Status": "passing"}, {"Status": "warning"}]
 	`)
 	var checks Checks
 	if err := json.Unmarshal(data, &checks); err != nil {
 		t.Error(err)
 	}
+	var actual State
 	for i, expected := range []State{StateAny, StateCritical, StatePassing, StateWarning} {
-		actual := checks[i].Status
+		actual = checks[i].Status
 		if expected != actual {
 			t.Errorf("main: expected type %d, got %d instead", expected, actual)
 		}
