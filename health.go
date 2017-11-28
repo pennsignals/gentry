89a87ddeeac66b9be808a14d70a3a407f602967e
rightlag <rightlag@gmail.com>
2017-11-28 10:36:06 -0500

    Update Check.ServiceTags field type

--- health.go
+++ health.go
@@ -32,15 +32,15 @@ const (
 )
 
 type Check struct {
-	ID          string `json:"CheckID"`
-	Name        string `json:"Name"`
-	Node        string `json:"Node"`
-	Notes       string `json:"Notes"`
-	Output      string `json:"Output"`
-	ServiceID   string `json:"ServiceID"`
-	ServiceName string `json:"ServiceName"`
-	ServiceTags string `json:"ServiceTags"`
-	Status      State  `json:"Status"`
+	ID          string   `json:"CheckID"`
+	Name        string   `json:"Name"`
+	Node        string   `json:"Node"`
+	Notes       string   `json:"Notes"`
+	Output      string   `json:"Output"`
+	ServiceID   string   `json:"ServiceID"`
+	ServiceName string   `json:"ServiceName"`
+	ServiceTags []string `json:"ServiceTags"`
+	Status      State    `json:"Status"`
 }
 
 type Checks []*Check
